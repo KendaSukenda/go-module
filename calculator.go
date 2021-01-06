8462edceb3bef5f78abe33fabeabe4784385f507
sukenda <soekenda09@gmail.com>
2021-01-06 10:05:16 +0700

    Add module

---
+++ calculator.go
@@ -0,0 +1,22 @@
+package go_module
+
+func Penjumlahan(a int, b int) int {
+	return a + b
+}
+
+func Pengurangan(a int, b int) int {
+	return a - b
+}
+
+func Perkalian(a int, b int) int {
+	return a * b
+}
+
+func Pembagian(a int, b int) int {
+	return a / b
+}
+
+func Modulo(a int, b int) int {
+	return a % b
+}
+
